c6cdcc6f969f7a49f0ce1a19bf51ad7fedfbc5e0
Richardkas-rek <richardlkas161210@gmail.com>
2026-03-16 21:14:43 +0200

    java activity

--- app/src/main/java/com/example/test11r16/MainActivity.java
+++ app/src/main/java/com/example/test11r16/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.test11r16;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText eAd;
+    private Button btnGo;
+    private WebView wV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eAd = findViewById(R.id.eAd);
+        btnGo = findViewById(R.id.btnGo);
+        wV = findViewById(R.id.wV);
+        wV.setWebViewClient(new WebViewClient());
+
+
+        wV.getSettings().setJavaScriptEnabled(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = eAd.getText().toString();
+
+                if(!url.startsWith("http://") && !url.startsWith("https://")){url = "https://" + url;}
+
+                wV.loadUrl(url);
+            }
+        });
     }
 }
